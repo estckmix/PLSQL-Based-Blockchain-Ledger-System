c4956b983b6e10f98447a9a0129a3bf3da1ebb0c
Dave R. <34291790+estckmix@users.noreply.github.com>
2025-03-15 19:11:01 -0400

    Create db_connection.sql

---
+++ config/db_connection.sql
@@ -0,0 +1,6 @@
+-- Database Connection Configuration
+DEFINE DB_USER = 'blockchain_admin';
+DEFINE DB_PASSWORD = 'securepassword';
+DEFINE DB_HOST = 'localhost';
+DEFINE DB_PORT = 1521;
+DEFINE DB_SERVICE_NAME = 'XE';
